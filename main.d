e3c76e8c94c0d748d7b3331b3083c1e69be9732e
Jayson Jia Yan <jj2yan@uwaterloo.ca>
2020-01-22 09:31:18 -0500

    Initial commit

---
+++ main.d
@@ -0,0 +1,8 @@
+main.o: main.c
+main.o: c:\Software\Keil\ARM\Inc\NXP\LPC17xx\LPC17xx.h
+main.o: c:\Software\Keil\ARM\CMSIS\Include\core_cm3.h
+main.o: c:\Software\Keil\ARM\ARMCC\bin\..\include\stdint.h
+main.o: c:\Software\Keil\ARM\CMSIS\Include\core_cmInstr.h
+main.o: c:\Software\Keil\ARM\CMSIS\Include\core_cmFunc.h
+main.o: c:\Software\Keil\ARM\Inc\NXP\LPC17xx\system_LPC17xx.h
+main.o: uart_polling.h
