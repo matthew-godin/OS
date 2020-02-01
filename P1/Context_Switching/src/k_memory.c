a8049e4eecba82668e71d93674b0fbdd64886b93
Matthew Godin <m2godin@edu.uwaterloo.ca>
2020-02-01 18:01:48 -0500

    comment

--- P1/Context_Switching/src/k_memory.c
+++ P1/Context_Switching/src/k_memory.c
@@ -16,6 +16,7 @@
 U32 *gp_stack; /* The last allocated stack low address. 8 bytes aligned */
                /* The first stack starts at the RAM high address */
 	       /* stack grows down. Fully decremental stack */
+		   // global pointer stack
 MEMORY_QUEUE gp_memory_queue;
 
 /**
